6ae3aedc8eaf8f8b9770cbf3bee927cd7fa024cb
Zac Colley <hi@zaccolley.com>
2016-10-16 00:18:36 +0100

    Added Ruben to examples

--- example.js
+++ example.js
@@ -1,5 +1,13 @@
 var letterboxd = require('./index');
 
+letterboxd('rubencordeiro')
+  .then(function(items) {
+    console.log(items);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
 letterboxd('zaccolley', function(error, items){
   if (error) {
     return console.log(error);
