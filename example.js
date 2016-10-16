b8aca07411a52c9547c094dcf5ae993f2ff01e46
Zac Colley <hi@zaccolley.com>
2016-10-16 01:53:53 +0100

    Updated README and example

--- example.js
+++ example.js
@@ -5,5 +5,28 @@ letterboxd('zaccolley', function(error, items){
     return console.log(error);
   }
 
-  console.log(items);
+  logItems(items);
 });
+
+function logItems(items) {
+  var diaryEntries = items.filter(item => item.type === 'diary');
+  var lists = items.filter(item => item.type === 'list');
+
+  console.log('');
+  console.log('Amount of diary entries: ' + diaryEntries.length);
+  console.log('Amount of lists: ' + lists.length);
+
+  console.log('\nDiary entries:\n');
+
+  diaryEntries.map(diaryEntry => {
+    console.log('  + ' + diaryEntry.film.title + ' (' + diaryEntry.uri + ')');
+  });
+
+  console.log('\nLists:\n');
+
+  lists.map(list => {
+    console.log('  + ' + list.title + ' (' + list.uri + ')');
+  });
+
+  console.log('');
+}
